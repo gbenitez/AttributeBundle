5f0f2b6456a097c48a519834bd9ed32c550b3c4b
franjs <franjs.francisco@gmail.com>
2016-06-16 15:07:03 -0400

    se definio la relacion con el owner del attributo por medio de una interface

---
+++ Entity/AttributeOwnerInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: franjs
+ * Date: 16/06/16
+ * Time: 02:15 PM
+ */
+
+namespace gbenitez\Bundle\AttributeBundle\Entity;
+
+
+interface AttributeOwnerInterface
+{
+
+}
