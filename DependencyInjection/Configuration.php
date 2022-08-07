5f8686c0df38eeb86f56d0ae830d68888d41bf8c
Gabriel Benitez <escorwer@gmail.com>
2022-08-06 23:43:40 -0400

    update

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder("attribute");
-        $rootNode = $treeBuilder->root();
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
