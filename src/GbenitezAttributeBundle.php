bea1d7d535e9bdc9885b8936963f8aa316bf4302
Gabriel Benitez <escorwer@gmail.com>
2022-08-09 02:23:02 -0400

    extension twig

--- src/GbenitezAttributeBundle.php
+++ src/GbenitezAttributeBundle.php
@@ -5,10 +5,16 @@ namespace gbenitez\Bundle\AttributeBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AttributeBundle extends Bundle
+class GbenitezAttributeBundle extends Bundle
 {
     public function getPath(): string
     {
         return \dirname(__DIR__);
     }
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+
+            }
 }
