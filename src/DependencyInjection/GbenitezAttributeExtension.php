a92ab41b1bbb2e081502df2948d39cea86911788
Gabriel Benitez <escorwer@gmail.com>
2022-08-21 16:11:51 -0400

    extension twig

--- src/DependencyInjection/GbenitezAttributeExtension.php
+++ src/DependencyInjection/GbenitezAttributeExtension.php
@@ -20,7 +20,7 @@ class GbenitezAttributeExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
-        $loader->load('services.yml');
+        $loader->load('services.yaml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
